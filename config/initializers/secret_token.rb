f918121d226aa93bf9b577c08a4142048ed357ab
root <root@abhas-Satellite-C640.(none)>
2012-12-04 17:51:45 +0530

    Initial changes

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+FirstApp::Application.config.secret_token = 'b4edde5b4ae39e655256419abfb15d35ce18af7e9a9a041cad0027678589b3909ab26d96055f6382b2badc32747ccc79b4bb420f371dac5fb8be27cae06e237e'
